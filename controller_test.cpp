8a25e17dc510ae2cf37836fb0b6ce6a192fcd14d
vgilja <vgilja@ucsd.edu>
2020-04-20 10:57:56 -0700

    Update controller_test.cpp

--- controller_test.cpp
+++ controller_test.cpp
@@ -2,8 +2,7 @@
 // Created by Shreyas Rajesh on 4/19/20.
 //
 #include "MadLibsController.hpp"
-#include "MadlibUserWordProvider.hpp"
-#include "MadlibAutoWordProvider.hpp"
+#include "MadlibWordProvider.hpp"
 
 using namespace std;
 
