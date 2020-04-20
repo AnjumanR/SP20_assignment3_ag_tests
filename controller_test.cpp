ddf4e4739c0d1abee6a260ebb3a217cae929ede5
vgilja <vgilja@ucsd.edu>
2020-04-20 13:11:59 -0700

    Update controller_test.cpp

--- controller_test.cpp
+++ controller_test.cpp
@@ -37,6 +37,7 @@ int main(int argc, const char * argv[]) {
         }
         else {
             cout << "FAILURE" << endl;
+            cout << "List: " << user_words << endl
             return 1;
         }
 
